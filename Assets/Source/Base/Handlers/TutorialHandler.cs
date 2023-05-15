aa08c15ea2e67afb6932455edc2ca0a9781b72b8
Mücahit Fatih Parlağı <58040833+Fatihprlg@users.noreply.github.com>
2023-05-15 21:35:54 +0300

    fixes

--- Assets/Source/Base/Handlers/TutorialHandler.cs
+++ Assets/Source/Base/Handlers/TutorialHandler.cs
@@ -1,7 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using DG.Tweening;
 using UnityEngine;
 
 public class TutorialHandler : MonoSingleton<TutorialHandler>, IInitializable, ICrossSceneObject
--- Assets/Source/Base/ViewModels/PopUpViewModel.cs
+++ Assets/Source/Base/ViewModels/PopUpViewModel.cs
@@ -6,14 +6,14 @@ using UnityEngine.Events;
 
 public class PopUpViewModel : ScreenElement
 {
-    public bool IsPopUpActive => activePopUp == null;
+    public bool IsPopUpActive => _activePopUp == null;
     [SerializeField] private List<PopUpModel> popUps;
     [SerializeField] private PopUpModel dummyPopUp;
     [SerializeField] private float timeBetweenPopUps;
     
-    private PopUpModel activePopUp;
-    private List<PopUpDataModel> popUpDatas;
-    private Queue<PopUpDataModel> popUpQueue;
+    private PopUpModel _activePopUp;
+    private List<PopUpDataModel> _popUpDatas;
+    private Queue<PopUpDataModel> _popUpQueue;
     public override void Initialize()
     {
         base.Initialize();
@@ -22,63 +22,80 @@ public class PopUpViewModel : ScreenElement
 
     public void ShowPopUp(int index, string header, string text, string button1Text, string button2Text, UnityAction onButton1Clicked, UnityAction onButton2Clicked)
     {
-        CreatePopUp(index, header, text, button1Text, onButton1Clicked, button2Text, onButton2Clicked);
-        ShowPopUp(index);
+        PopUpDataModel data = new PopUpDataModel()
+        {
+            Index = index,
+            Header = header,
+            Text = text,
+            Button1Text = button1Text,
+            Button2Text = button2Text,
+            Button1Action = onButton1Clicked,
+            Button2Action = onButton2Clicked
+        };
+        ShowPopUp(data);
     }
     
     public void ShowPopUp(int index, string header, string text, string buttonText, UnityAction onButtonClicked)
     {
-        CreatePopUp(index, header, text, buttonText, onButtonClicked);
-        ShowPopUp(index);
+        PopUpDataModel data = new PopUpDataModel()
+        {
+            Index = index,
+            Header = header,
+            Text = text,
+            Button1Text = text,
+            Button1Action = onButtonClicked,
+        };
+        ShowPopUp(data);
     }
 
     public PopUpDataModel ShowPopUp(int index)
     {
-        var data = popUpDatas.FirstOrDefault(p => p.Index == index);
+        var data = _popUpDatas.FirstOrDefault(p => p.Index == index);
         if(data == null)
             return null;
-        popUpQueue.Enqueue(data);
-        if(popUpQueue.Count <= 1) StartCoroutine(N_ShowPopUp());
+        _popUpQueue.Enqueue(data);
+        if(_popUpQueue.Count <= 1) StartCoroutine(N_ShowPopUp());
         return data;
     }
     public void ShowPopUp(PopUpDataModel popUp)
     {
-        popUpQueue.Enqueue(popUp);
+        _popUpQueue.Enqueue(popUp);
     }
 
     public void HideActivePopUp()
     {
-        activePopUp.Hide();
-        activePopUp = null;
+        _activePopUp.Hide();
+        _activePopUp = null;
     }
     
     public void HidePopUp(int index)
     {
-        var popUp = popUps.FirstOrDefault(p => p.GetData().Index == index).GetData();
-        HidePopUp(popUp);
+        var popUp = popUps.FirstOrDefault((p) => p.GetData().Index == index);
+        if (popUp != null) popUp.Hide();
+        _activePopUp = null;
     }
 
     public void HidePopUp(PopUpDataModel data)
     {
         var popUp = popUps.FirstOrDefault((p) => p.GetData().Index == data.Index);
-        popUp.Hide();
-        activePopUp = null;
+        if (popUp != null) popUp.Hide();
+        _activePopUp = null;
     }
 
-    IEnumerator N_ShowPopUp()
+    private IEnumerator N_ShowPopUp()
     {
         yield return null;
-        while (popUpQueue.Count > 0)
+        while (_popUpQueue.Count > 0)
         {
-            var data = popUpQueue.Dequeue();
+            var data = _popUpQueue.Dequeue();
             var popUp = popUps.FirstOrDefault(p => p.GetData().Index == data.Index);
-            if(popUp == null)
+            if(popUp is null)
             {
                 dummyPopUp.SetData(data);
                 popUp = dummyPopUp;
             }
-            activePopUp = popUp;
-            activePopUp.Show();
+            _activePopUp = popUp;
+            _activePopUp.Show();
             yield return new WaitUntil(() => !IsPopUpActive);
             yield return new WaitForSeconds(timeBetweenPopUps);
         }
@@ -91,27 +108,12 @@ public class PopUpViewModel : ScreenElement
 
     private void GetAllPopUpDatas()
     {
-        popUpDatas = new List<PopUpDataModel>();
-        popUpDatas.Clear();
+        _popUpDatas = new List<PopUpDataModel>();
+        _popUpDatas.Clear();
         foreach (PopUpModel popUpModel in popUps)
         {
-            popUpDatas.Add(popUpModel.GetData());
+            _popUpDatas.Add(popUpModel.GetData());
         }
     }
     
-    public PopUpDataModel CreatePopUp(int index, string header, string text, string button1Text, UnityAction onButton1Clicked, string button2Text = "", UnityAction onButton2Clicked = null)
-    {
-        PopUpDataModel data = new PopUpDataModel()
-        {
-            Index = index,
-            Header = header,
-            Text = text,
-            Button1Text = button1Text,
-            Button2Text = button2Text,
-            Button1Action = onButton1Clicked,
-            Button2Action = onButton2Clicked
-        };
-        data.SetData(data);
-        return data;
-    }
 }
